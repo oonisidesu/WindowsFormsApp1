6f6495fd3e4985e04ce5bc9137d0e45fffc332e0
由幸 大坪 <eeeelllleeee24@gmail.com>
2020-07-24 22:47:31 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/WeatherLatestView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class WeatherLatestView : Form
+    {
+        private readonly string ConnectionString = @"D:\SQLite\20200525_Udemy\DDD.db;Version=3;";
+        public WeatherLatestView()
+        {
+            InitializeComponent();
+        }
+
+        private void LatestButton_Click(object sender, EventArgs e)
+        {
+            string sql = @"
+select DataDate,
+       Condition,
+       Tempareture
+from Weather
+where AreaId = @AreaId
+order by DataDate desc
+LIMIT 1"
+;
+            DataTable dt = new DataTable();
+            using(var connection = new SQLiteConnection(ConnectionString))
+            using(var command = new SQLiteCommand(sql, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@AreaId", this.AreaIdTextBox.Text);
+
+            }
+        }
+    }
+}
