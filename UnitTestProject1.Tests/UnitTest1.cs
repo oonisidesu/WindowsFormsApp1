484d5fc4a45173cde96c015c0b9dcbe3d2dd7257
由幸 大坪 <eeeelllleeee24@gmail.com>
2020-07-28 23:04:49 +0900

    テストコードを作成

---
+++ UnitTestProject1.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1.Common;
+
+namespace UnitTestProject1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var val = Class1.Add(1, 2);
+            Assert.AreEqual(3, val);
+        }
+    }
+}
