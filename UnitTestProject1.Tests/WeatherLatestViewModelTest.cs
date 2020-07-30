8c2120e23707018b499f8df170b9c914064285e8
由幸 大坪 <eeeelllleeee24@gmail.com>
2020-07-30 23:30:44 +0900

    テストを追加

---
+++ UnitTestProject1.Tests/WeatherLatestViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1.ViewModels;
+
+namespace UnitTestProject1.Tests
+{
+    [TestClass]
+    public class WeatherLatestViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+            var viewModel = new WeatherLatestViewModel();
+            Assert.AreEqual("", viewModel.AreaIdText);
+            Assert.AreEqual("", viewModel.DataDateText);
+            Assert.AreEqual("", viewModel.ConditionText);
+            Assert.AreEqual("", viewModel.TemperatureText);
+        }
+    }
+}
